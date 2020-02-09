6f9e5f0acf288ce15970f4e8274bbd61368c5994
rafa-ela <raftabay@gmail.com>
2020-02-09 22:52:00 +1300

    use logger

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/dota2-scraper
 
 go 1.12
 
-require github.com/PuerkitoBio/goquery v1.5.0
+require (
+	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.4.2
+)
---
+++ scraper/util.go
@@ -0,0 +1,29 @@
+package scraper
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+)
+
+// WriteDataToFileAsJSON is taken from: https://stackoverflow.com/a/57192522
+func WriteDataToFileAsJSON(data interface{}, filedir string) (int, error) {
+	//write data as buffer to json encoder
+	buffer := new(bytes.Buffer)
+	encoder := json.NewEncoder(buffer)
+	encoder.SetIndent("", "\t")
+
+	err := encoder.Encode(data)
+	if err != nil {
+		return 0, err
+	}
+	file, err := os.OpenFile(filedir, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		return 0, err
+	}
+	n, err := file.Write(buffer.Bytes())
+	if err != nil {
+		return 0, err
+	}
+	return n, nil
+}
